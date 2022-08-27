0b0a5553ca4315d451657aa5485a7b134c4e26ad
tyler-cranmer <tyler.cranmer@outlook.com>
2022-08-27 14:55:34 -0600

    deleted a few files that are not needed

--- src/Components/CustomSVGs/MetaMaskSvg.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { SvgIcon } from '@mui/material'
-import metaMaskIcon from '../../images/walletIcons/metamask.svg';
-
-function MetaMaskSvg() {
-  return <SvgIcon component={metaMaskIcon} inheritViewBox />;
-}
-
-export default MetaMaskSvg
