7f25e8454c7dc837b6542edf58aa39d7e25efe87
tyler-cranmer <tyler.cranmer@outlook.com>
2022-09-04 14:47:30 -0600

    changing exports

--- src/Components/main/index.ts
+++
@@ -1,6 +0,0 @@
-export { default as Blogs } from './Blogs';
-export { default as Community } from './Community';
-export { default as CustomCard } from './CustomCard';
-export { default as Explore } from './Explore';
-export { default as Hero } from './Hero';
-export { default as Products } from './Products';
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,11 @@ import * as React from 'react';
 import { useState } from 'react';
 import type { NextPage } from 'next';
 import { Container } from '@mui/material';
-import { Hero, Products, Community, Blogs, Explore } from '../components/main';
-
-
+import Hero from '../components/main/Hero';
+import Products from '../components/main/Products';
+import Community from '../components/main/Community';
+import Blogs  from '../components/main/Blogs';
+import Explore from '../components/main/Explore';
 
 const Home: NextPage = () => {
   return (
