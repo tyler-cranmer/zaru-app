1bf26303df4aa8fd403a9e31c37d99239ac34d1b
tyler-cranmer <tyler.cranmer@outlook.com>
2022-09-04 14:34:08 -0600

    webpack

--- next.config.js
+++ next.config.js
@@ -3,15 +3,22 @@
 module.exports = {
   reactStrictMode: true,
 
-};
-
-module.exports = {
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
       use: ['@svgr/webpack'],
     });
-
     return config;
   },
 };
+
+// module.exports = {
+//   webpack(config) {
+//     config.module.rules.push({
+//       test: /\.svg$/,
+//       use: ['@svgr/webpack'],
+//     });
+
+  //   return config;
+  // },
+// };
