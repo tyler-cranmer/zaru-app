e6a8dc1722101a44b22f33bf4f8b37dc541f7093
tyler-cranmer <tyler.cranmer@outlook.com>
2022-09-04 15:09:35 -0600

    comment out

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,20 +2,20 @@ import * as React from 'react';
 import { useState } from 'react';
 import type { NextPage } from 'next';
 import { Container } from '@mui/material';
-import Hero from '../components/main/Hero';
-import Products from '../components/main/Products';
-import Community from '../components/main/Community';
-import Blogs  from '../components/main/Blogs';
-import Explore from '../components/main/Explore';
+// import Hero from '../components/main/Hero';
+// import Products from '../components/main/Products';
+// import Community from '../components/main/Community';
+// import Blogs  from '../components/main/Blogs';
+// import Explore from '../components/main/Explore';
 
 const Home: NextPage = () => {
   return (
     <>
-      <Hero />
+      {/* <Hero />
       <Products />
       <Community />
       <Blogs />
-      <Explore />
+      <Explore /> */}
     </>
   );
 };
