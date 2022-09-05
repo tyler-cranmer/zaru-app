a0be46d9fc8a38f0e77e527e803d859d83e2f02e
tyler-cranmer <tyler.cranmer@outlook.com>
2022-09-04 18:25:08 -0600

    chagned some setttings

--- next.config.js
+++ next.config.js
@@ -1,24 +1,4 @@
 /** @type {import('next').NextConfig} */
-
 module.exports = {
   reactStrictMode: true,
-
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack'],
-    });
-    return config;
-  },
 };
-
-// module.exports = {
-//   webpack(config) {
-//     config.module.rules.push({
-//       test: /\.svg$/,
-//       use: ['@svgr/webpack'],
-//     });
-
-  //   return config;
-  // },
-// };
